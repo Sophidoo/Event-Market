41bca8d910a6b790c546b7881355c54b75734edc
Sophia Okosodo <63817894+Sophidoo@users.noreply.github.com>
2025-06-03 22:54:57 +0100

    home page in progress

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
-import { HeartIcon } from '@heroicons/react/24/outline'
+import { ChevronLeftIcon, ChevronRightIcon, HeartIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, HomeIcon, CalendarDateRangeIcon, BriefcaseIcon, InboxStackIcon, Squares2X2Icon, ClipboardDocumentCheckIcon, UserCircleIcon, Bars3Icon, MagnifyingGlassIcon} from '@heroicons/react/24/solid'
 import "../styles/componentStyle/Navbar.css"
 import "../styles/Home.css"
 import { NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Home = () => {
     const [showMenu, setShowMenu] = useState(false)
@@ -15,6 +22,22 @@ const Home = () => {
         end_date: "",
         location: ""
     })
+    
+    const swiperRef = useRef();
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     const decideMenuToShow = (decision) => {
         if(decision === "menu"){
@@ -27,7 +50,9 @@ const Home = () => {
     }
     const handleSearch = (e) => {
         e.preventDefault();
-        setLoading(true)
+        navigate("/rentals", {
+            state: searchDetails
+        });
     }
 
     const handleChange = (e) => {
@@ -39,7 +64,7 @@ const Home = () => {
     };
     return <>
         <section className="homeHeroSection">
-            <nav className="text-white flex justify-between">
+            <nav className={`text-white flex justify-between ${scrolled ? "bg-[#0B5850]" : ""}`}>
                 <div className="leftNav">
                     <img src="" alt="" />
                     <ul>
@@ -137,7 +162,7 @@ const Home = () => {
                     <label htmlFor=""  className='text-gray-600'>
                         RENT START DATE
                     </label>
-                    <input type="date" name="start_date" id="" value={searchDetails.start_date} onChange={handleChange}/>
+                    <input type="date" name="start_date" id="" value={searchDetails.start_date} onChange={handleChange} placeholder='dd/mm/yyyy'/>
                 </div>
                 <div className="inputWrapper border-r-gray-200">
                     <label htmlFor=""  className='text-gray-600'>
@@ -160,7 +185,78 @@ const Home = () => {
         </section>
 
         <section className='itemsWrapper'>
-            
+            <div className="itemsHolder">
+                <div className="itemsWrapperHeading">
+                    <h2>Most Booked Rentals <ChevronRightIcon/></h2>
+                    <div className="itemsArrow">
+                        <ChevronLeftIcon className='border-gray-300 text-gray-400 hover:bg-gray-200'  onClick={() => swiperRef.current?.slidePrev()}/>
+                        <ChevronRightIcon className='border-gray-300 text-gray-400 hover:bg-gray-200'  onClick={() => swiperRef.current?.slideNext()}/>
+                    </div>
+                </div>
+                <Swiper
+                    modules={[Navigation, A11y]}
+                    spaceBetween={30}
+                    className="mySwiper"
+                    slidesPerView={5}
+                    onBeforeInit={(swiper) => {
+                        swiperRef.current = swiper;
+                    }}
+                    breakpoints={{
+                        // when the window width is >= 320px
+                        100: {
+                        slidesPerView: 1.5,
+                        spaceBetween: 20,
+                        },
+                        500: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        },
+                        // when the window width is >= 640px
+                        650: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                        },
+                        // when the window width is >= 1024px
+                        1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 40,
+                        },
+                    }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')}
+                >
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className="itemCard">
+                            <img src="" alt="" />
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </section>
     </>
 
