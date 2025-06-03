6b117a319f4bbe6df669ad4a98aad0b8c20e23fe
Sophia Okosodo <63817894+Sophidoo@users.noreply.github.com>
2025-06-03 20:36:08 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
 import { HeartIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, HomeIcon, CalendarDateRangeIcon, BriefcaseIcon, InboxStackIcon} from '@heroicons/react/24/solid'
+import { ChevronDownIcon, HomeIcon, CalendarDateRangeIcon, BriefcaseIcon, InboxStackIcon, Squares2X2Icon, ClipboardDocumentCheckIcon, UserCircleIcon, Bars3Icon} from '@heroicons/react/24/solid'
 import "../styles/componentStyle/Navbar.css"
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+    const [showResponsiveMenu, setShowResponsiveMenu] = useState(false)
+
+    const decideMenuToShow = (decision) => {
+        if(decision === "menu"){
+            setShowMenu(!showMenu)
+            setShowResponsiveMenu(false)
+        }else if(decision === "responsive"){
+            setShowResponsiveMenu(!showResponsiveMenu)
+            setShowMenu(false)
+        }
+    }
     
     return<>
         <nav className="text-white bg-[#0B5850] flex justify-between">
             <div className="leftNav">
                 <img src="" alt="" />
                 <ul>
-                    <li className='hover:bg-white hover:text-[#0B5850]'><HomeIcon/> Home</li>
-                    <li className='hover:bg-white hover:text-[#0B5850]'><CalendarDateRangeIcon/> Rentals</li>
-                    <li className='hover:bg-white hover:text-[#0B5850]'><BriefcaseIcon/> Services</li>
-                    <li className='hover:bg-white hover:text-[#0B5850]'><InboxStackIcon/> Packages</li>
+                    <NavLink><li className='hover:bg-white hover:text-[#0B5850]'><HomeIcon/> Home</li></NavLink>
+                    <NavLink><li className='hover:bg-white hover:text-[#0B5850]'><CalendarDateRangeIcon/> Rentals</li></NavLink>
+                    <NavLink><li className='hover:bg-white hover:text-[#0B5850]'><BriefcaseIcon/> Services</li></NavLink>
+                    <NavLink><li className='hover:bg-white hover:text-[#0B5850]'><InboxStackIcon/> Packages</li></NavLink>
                 </ul>
             </div>
 
@@ -21,14 +35,48 @@ const Navbar = () => {
 
                 <div className="userProfile bg-[#136B61]">
                     <img   src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?uid=R95769918&ga=GA1.1.837528501.1678343204&semt=ais_items_boosted&w=740" alt="" />
-                    <ChevronDownIcon/>
+                    <ChevronDownIcon className='cursor-pointer' onClick={() => decideMenuToShow("menu")}/>
+                    <div className={showMenu ? "navMenu" : "hide" }>
+                        <NavLink 
+                            className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                        ><Squares2X2Icon/> Dashboard</NavLink>
+                        {/* <hr className='border-[0.5px] border-gray-100'/> */}
+                        <NavLink 
+                            className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                        ><ClipboardDocumentCheckIcon/> Bookings</NavLink>
+                        {/* <hr className='border-[0.5px] border-gray-100'/> */}
+                        <NavLink
+                            className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                        ><HeartIcon/> Wishlist</NavLink>
+                        {/* <hr className='border-[0.5px] border-gray-100'/> */}
+                        <NavLink
+                            className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                        ><UserCircleIcon/> Profile</NavLink>
+                    </div>
                 </div>
+
+                <Bars3Icon onClick={() => decideMenuToShow("responsive")} className='menuIcon cursor-pointer'/>
             </div>
 {/* 
             <div className="rightNavButton flex gap-1 align-middle">
                 <button className='underline'>Create an account</button>
                 <button className='border-[1px] border-white rounded-sm text-[#073732] px-3 py-1 text-white'>Login</button>
             </div> */}
+
+            <div className={showResponsiveMenu && window.innerWidth <= 870 ? "navMenu navMenu2" : "hide"}>
+                <NavLink
+                    className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                ><HomeIcon/> Home</NavLink>
+                <NavLink
+                    className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                ><CalendarDateRangeIcon/> Rentals</NavLink>
+                <NavLink
+                    className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                ><BriefcaseIcon/> Services</NavLink>
+                <NavLink
+                    className="text-[#0B5850] hover:bg-[rgb(11, 84, 76, 0.11)]"
+                ><InboxStackIcon/> Packages</NavLink>
+            </div>
         </nav>
     </>
 
