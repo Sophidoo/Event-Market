b7b1e68bf35e00a42a93420db3294069e05f80cc
Sophia Okosodo <63817894+Sophidoo@users.noreply.github.com>
2025-06-14 13:02:50 +0100

    dashboard

--- src/Pages/dashboard/Overview.jsx
+++ src/Pages/dashboard/Overview.jsx
@@ -1,8 +1,245 @@
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import "../../styles/dashboard/overview.css";
 import { DocumentTextIcon } from "@heroicons/react/16/solid";
+import Chart from "react-apexcharts";
 
 const Overview = () => {
+
+  const area = {
+    options: {
+      chart: {
+        type: "area",
+        stacked: true,
+        toolbar: { show: false },
+        zoom: { enabled: false },
+      },
+      stroke: {
+        curve: "smooth",
+        width: 2,
+        colors: ["#0B544C"], // Outline color for both series
+      },
+      colors: ["#0B544C"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "dark",
+          type: "vertical", // vertical gradient for area chart
+          shadeIntensity: 1,
+          gradientToColors: ["#128D7F"], // ending color
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 0,
+          stops: [0, 100]
+        }// Fill colors for stacked series
+      },
+      markers: {
+        size: 0,
+        colors: ["#79DA11"],
+        strokeColors: "#fff",
+        strokeWidth: 2,
+      },
+      xaxis: {
+        categories: [
+          "JAN",
+          "FEB",
+          "MAR",
+          "APR",
+          "MAY",
+          "JUN",
+          "JUL",
+          "AUG",
+          "SEP",
+          "OCT",
+          "NOV",
+          "DEC",
+        ], // Example X values
+        labels: {
+          style: {
+            colors: "#52525B",
+            fontSize: "11.5px",
+            fontFamily: "Inter, sans-serif",
+            leterSpacing: "0px", // Optional: x-axis label color
+          },
+        },
+        crosshairs: {
+          show: true,
+          stroke: {
+            color: "#E4E4E7",
+            width: 1,
+            dashArray: 0, // ✅ solid line
+          },
+        },
+        tooltip: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      yaxis: {
+        show: true, // Hide Y-axis
+        lines: {
+          show: true,
+        },
+      },
+      tooltip: {
+        enabled: true,
+        style: {
+          fontSize: "10px", // optional font size
+          fontFamily: undefined, // optional font family
+          color: "#79DA11", // background color
+        },
+        custom: ({ series, dataPointIndex, w }) => {
+          const names = w.globals.seriesNames;
+          const tooltips = series
+            .map((s, i) => {
+              const value = s[dataPointIndex];
+              return `
+                        <div style="
+                          margin-bottom: 4px;
+                          color: #71717A;
+                          font-size: 12px;
+                          font-family: 'Plus Jakarta Sans', sans-serif;
+                        ">
+                          ${names[i]}: ${value}
+                        </div>
+                      `;
+            })
+            .join("");
+
+          return `
+                      <div style="
+                        background: #fff;
+                        padding: 8px 13px;
+                        border: 2px solid #ffffff;
+                        border-radius: 4px;
+                        width: max-content;
+                        box-shadow: 0 2px 6px rgba(0,0,0,0.08);
+                        position: relative;
+                      ">
+                        ${tooltips}
+                        <div style="
+                          position: absolute;
+                          bottom: -6px;
+                          left: 50%;
+                          transform: translateX(-50%);
+                          width: 0;
+                          height: 0;
+                          border-left: 6px solid transparent;
+                          border-right: 6px solid transparent;
+                          border-top: 6px solid white;
+                        "></div>
+                      </div>
+                    `;
+        },
+      },
+      grid: {
+        show: true,
+        borderColor: "#F4F4F5", // Optional: hide grid lines
+      },
+      legend: {
+        show: false, // Optional: hide legend
+      },
+    },
+    series: [
+      {
+        name: "Series 1",
+        data: [10, 20, 15, 30, 25, 40, 5, 10, 10, 15, 10, 20],
+      }
+    ],
+  };
+  const donut = {
+    options: {
+      chart: {
+        type: "donut",
+        offsetY: 20, // push chart up to visually center it in semi mode
+      },
+      colors: ["#0B544C", "#1DC9B5", "#128D7F"],
+      labels: ["Rentals", "Services", "Packages"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              offsetY: 0, // reset offset for smaller screens
+            },
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      ],
+      legend: {
+        show: false,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 4, // spacing between segments
+        colors: ['#fff'], // or match your background color
+      },
+      plotOptions: {
+        pie: {
+          startAngle: -90,
+          endAngle: 90,
+          offsetY: 10,
+          expandOnClick: false,
+          donut: {
+            size: "65%",
+            labels: {
+              show: true,
+              name: {
+                show: true,
+              },
+              value: {
+                show: false,
+                fontSize: "30px",
+                fontWeight: 600,
+                padding: "0px",
+                color: "#18181B",
+                offsetY: 10,
+                formatter: function (val) {
+                  return `${val}%`;
+                },
+              },
+              total: {
+                show: true,
+                label: '',
+                color: '#18181B',
+                formatter: function (w) {
+                  return `${w.globals.series[0]}%`;
+                },
+              },
+            },
+          },
+        },
+      },
+      tooltip: {
+        enabled: true,
+      },
+      responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          plotOptions: {
+            pie: {
+              donut: {
+                labels: {
+                  value: {
+                    fontSize: "20px",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ],
+    },
+    series: [42.2, 27.8, 30],
+  };
     
   return <>
     <div className="overviewContainer">
@@ -63,9 +300,36 @@ const Overview = () => {
       <div className="overviewChartWrapper">
         <div className="revenueWrapper border-gray-200 border-[1px] bg-white">
           <h2>Revenue</h2>
+
+           <div className="revenueChartScroll">
+              <div className="revenueChart">
+                <Chart
+                  options={area.options}
+                  series={area.series}
+                  type="area"
+                  height={250}
+                  width="100%"
+                />
+
+                
+              </div>
+            </div>
         </div>
 
         <div className="donutWrapper border-gray-200 border-[1px] bg-white">
+
+          <div className="donutChart">
+            <Chart
+              options={donut.options}
+              series={donut.series}
+              type="donut"
+              width="100%"
+            />
+            <div className="chartTotal">
+                  <h1>459</h1>
+                  <p className="text-gray-600">Total Inventory</p>
+                </div>
+          </div>
 
           <div className="donutWrapperDetails">
 
